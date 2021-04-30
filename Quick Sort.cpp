0990f040935088cfc713919764d0fabb32eb744a
NAMAN JAIN <53976820+JNAMAN243@users.noreply.github.com>
2021-04-30 10:37:52 +0530

    Update Quick Sort.cpp

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -1,65 +1,89 @@
-#include<iostream>
+
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
 using namespace std;
-int comp=0;
-void quicksort(int a[], int i, int j);
-int partition(int a[],int l,int u);
-int main()
+
+int comp =0;
+
+int partition(int arr[], int low, int high)
 {
-	int n,t,j;
-	cout<<"\nEnter the size of array:\n";
-	cin>>n;
-	int ar[n];
-	cout<<"\nEnter elements in an array:\n";
-	for(int i=0;i<n;i++)
+	int pivot = arr[high];
+
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++)
 	{
-		cin>>ar[i];
-	}
-	cout<<"\nElements are:\n";
-		for(int i=0;i<n;i++)
-	{
-		cout<<ar[i]<<" ";
-	}
-	quicksort(ar,0,n-1);
-	cout<<"\nsorted array is:\n";
-	for(int i=0;i<n;i++)
-	{
-		cout<<" "<<ar[i];
-	}
-	cout<<"\nTotal number of comparisons are: "<<comp;
-	return 0;
-}
-void quicksort(int a[],int i,int j)
-{
-	int mid;
-	if(i<j)
-	{
-	mid=partition(a,i,j);
-	quicksort(a,i, mid-1);
-	quicksort(a,mid+1,j);	
-	}
-}
-int partition(int a[],int l,int u)
-{
-	int p,i,j,temp;
-	p=a[l];
-	i=l;
-	j=u+1;
-	do{
-		do
-		i++;
-		while(a[i]<p && i<=u);
-		do
-		j--;
-		while(p<a[j]);
-		if(i<j)
-		{
-		 temp=a[i];
-		 a[i]=a[j];
-		 a[j]=temp;
+		
+		if (arr[j] <= pivot) {
+			i++;
+			swap(arr[i], arr[j]);
 		}
 		comp++;
-	}while(i<j);
-	a[l]=a[j];
-	a[j]=p;
-	return(j);
+	}
+	swap(arr[i + 1], arr[high]);
+	return (i + 1);
+}
+
+
+int partition_r(int arr[], int low, int high)
+{
+	
+	srand(time(NULL));
+	int random = low + rand() % (high - low);
+	
+	swap(arr[random], arr[high]);
+
+	return partition(arr, low, high);
+}
+
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high) {
+
+		int pi = partition_r(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+int main()
+{
+
+	int size,ext;
+  begin:
+  	comp=0;
+	cout<<"Enter the size of array : ";
+	cin>>size;
+	int arr[size];
+	cout<<"\n\n";
+	for(int i=0;i<size;i++)
+	{
+		cout<<"Enter element "<<i+1<<" in an array : ";
+		cin>>arr[i];
+	}
+	
+    cout << "\n\nGiven array is :    ";
+	printArray(arr, size);
+	
+	quickSort(arr, 0, size - 1);
+
+	cout<<"\n\nSorted array is:   ";
+	printArray(arr, size);
+	cout<<"\nNo of comparisons are :  "<<comp;
+	cout<<"\n\nPress 1 to start again / any other key to exit : ";
+  	cin>>ext;
+  	if(ext == 1)
+     	goto begin;
+	return 0;
 }
