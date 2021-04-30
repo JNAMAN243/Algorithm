177bb97de2775e5d7978822c1b762e490bf7ffff
NAMAN JAIN <53976820+JNAMAN243@users.noreply.github.com>
2021-04-30 10:38:20 +0530

    Update Radix Sort.cpp

--- Radix Sort.cpp
+++ Radix Sort.cpp
@@ -1,68 +1,74 @@
 #include <iostream>
- 
 using namespace std;
- 
-// Get maximum value from array.
 
- 
-// Count sort of arr[].
+int getMax(int arr[], int n)
+{
+	int mx = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] > mx)
+			mx = arr[i];
+	return mx;
+}
+
 void countSort(int arr[], int n, int exp)
 {
-	// Count[i] array will be counting the number of array values having that 'i' digit at their (exp)th place.  
-	int output[n], i, count[10] = {0};
- 
-	// Count the number of times each digit occurred at (exp)th place in every input.
+	int output[n]; 
+	int i, count[10] = { 0 };
 	for (i = 0; i < n; i++)
 		count[(arr[i] / exp) % 10]++;
- 
-	// Calculating their cumulative count.
+
 	for (i = 1; i < 10; i++)
-		count[i] += count[i-1];
- 
-	// Inserting values according to the digit '(arr[i] / exp) % 10' fetched into count[(arr[i] / exp) % 10].
-	for (i = n - 1; i >= 0; i--)
-	{
+		count[i] += count[i - 1];
+
+	for (i = n - 1; i >= 0; i--) {
 		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
 		count[(arr[i] / exp) % 10]--;
 	}
- 
-	// Assigning the result to the arr pointer of main().
+	
 	for (i = 0; i < n; i++)
 		arr[i] = output[i];
 }
- 
-// Sort arr[] of size n using Radix Sort.
+
 void radixsort(int arr[], int n)
 {
-	int exp;
-	int max = arr[0];
-	for (int i = 1; i < n; i++)
-		if (arr[i] > max)
-			max = arr[i];
- 
-	// Calling countSort() for digit at (exp)th place in every input.
-	for (exp = 1; max/exp > 0; exp *= 10)
+
+	int m = getMax(arr, n);
+
+	for (int exp = 1; m / exp > 0; exp *= 10)
 		countSort(arr, n, exp);
 }
- 
+
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
 int main()
 {
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
- 
-	int arr[n];
-	for(i = 0; i < n; i++)
+		int size,ext;
+  begin:
+	cout<<"Enter the size of array : ";
+	cin>>size;
+	int arr[size];
+	cout<<"\n\n";
+	for(int i=0;i<size;i++)
 	{
-		cout<<"Enter element "<<i+1<<": ";
+		cout<<"Enter element "<<i+1<<" in an array : ";
 		cin>>arr[i];
 	}
- 
-	radixsort(arr, n);
- 
-	// Printing the sorted data.
-	cout<<"\nSorted Data ";
-	for (i = 0; i < n; i++)
-		cout<<"->"<<arr[i];
+	
+    cout << "\n\nGiven array is :    ";
+	printArray(arr, size);
+	
+	radixsort(arr, size);
+
+	cout<<"\n\nSorted array is:   ";
+	printArray(arr, size);
+	cout<<"\n\nPress 1 to start again / any other key to exit : ";
+  	cin>>ext;
+  	if(ext == 1)
+     	goto begin;
 	return 0;
 }
