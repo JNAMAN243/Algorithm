68a6bb868138ff304258456aa1c87f30bd6c5bcd
NAMAN JAIN <53976820+JNAMAN243@users.noreply.github.com>
2021-04-30 10:32:35 +0530

    Update Heap Sort.cpp

--- Heap Sort.cpp
+++ Heap Sort.cpp
@@ -1,76 +1,80 @@
 #include <iostream>
- 
+
 using namespace std;
-int comp=0;
- 
-// A function to heapify the array.
-void MaxHeapify(int a[], int i, int n)
+
+int comp =0;
+
+void heapify(int arr[], int n, int i)
 {
-	int j, temp;
-	temp = a[i];
-	j = 2*i;
- 
- 	while (j <= n)
-	{
-		if (j < n && a[j+1] > a[j])
-		j = j+1;
-		// Break if parent value is already greater than child value.
-		if (temp > a[j])
-			break;
-		// Switching value with the parent node if temp < a[j].
-		else if (temp <= a[j])
-		{
-			a[j/2] = a[j];
-			j = 2*j;
-		}
+	int largest = i; // Initialize largest as root
+	int l = 2 * i + 1; // left = 2*i + 1
+	int r = 2 * i + 2; // right = 2*i + 2
+
+	// If left child is larger than root
+	if (l < n ){
+		if(arr[l] > arr[largest])
+		largest = l;
+		comp++;
 	}
-	a[j/2] = temp;
-	comp++;
-	return;
-}
-void HeapSort(int a[], int n)
-{
-	int i, temp;
-	for (i = n; i >= 2; i--)
-	{
-		// Storing maximum value at the end.
-		temp = a[i];
-		a[i] = a[1];
-		a[1] = temp;
-		
-		
-		// Building max heap of remaining element.
-		MaxHeapify(a, 1, i - 1);
+	// If right child is larger than largest so far
+	if (r < n  ){
+		if(arr[r] > arr[largest])
+		largest = r;
+		comp++;
 	}
 	
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+
+		heapify(arr, n, largest);
+	}
 }
-void Build_MaxHeap(int a[], int n)
+
+void heapSort(int arr[], int n)
 {
-	int i;
-	for(i = n/2; i >= 1; i--)
-		MaxHeapify(a, i, n);
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+
+	for (int i = n - 1; i > 0; i--) {
+		swap(arr[0], arr[i]);
+		heapify(arr, i, 0);
+	}
 }
+
+
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
 int main()
 {
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
-	n++;
-	int arr[n];
-	for(i = 1; i < n; i++)
+	int size,ext;
+  begin:
+  	comp=0;
+	cout<<"Enter the size of array : ";
+	cin>>size;
+	int arr[size];
+	cout<<"\n\n";
+	for(int i=0;i<size;i++)
 	{
-		cout<<"\nEnter element "<<i<<": ";
+		cout<<"Enter element "<<i+1<<" in an array : ";
 		cin>>arr[i];
 	}
-	// Building max heap.
-	Build_MaxHeap(arr, n-1);
-	HeapSort(arr, n-1);
- 
-	// Printing the sorted data.
-	cout<<"\nSorted Data ";
- 
-	for (i = 1; i < n; i++)
-		cout<<"->"<<arr[i];
-    cout<<"\nThe total number of comparisions are:"<<comp;
+	
+    cout << "\n\nGiven array is :    ";
+	printArray(arr, size);
+	
+	heapSort(arr, size);
+
+	cout<<"\n\nSorted array is:   ";
+	printArray(arr, size);
+	cout<<"\nNo of comparisons are :  "<<comp;
+	cout<<"\n\nPress 1 to start again / any other key to exit : ";
+  	cin>>ext;
+  	if(ext == 1)
+     	goto begin;
 	return 0;
 }
